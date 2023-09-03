8bd67065ef8b22028ae69c3432e58d862afbeb8e
hyunwoo <wjdgusdndi@gmail.com>
2023-09-03 23:45:51 +0900

    Feat : find account

--- solup/src/main/java/com/example/solup/service/account/AccountService.java
+++ solup/src/main/java/com/example/solup/service/account/AccountService.java
@@ -1,9 +1,12 @@
 package com.example.solup.service.account;
 
+import com.example.solup.dto.AccountDto;
 import com.example.solup.dto.TradeHistoryDto;
 import com.example.solup.entity.TradeHistory;
+import com.example.solup.entity.User;
 import com.example.solup.repository.account.AccountRepository;
 import com.example.solup.repository.account.TradeHistoryRepository;
+import com.example.solup.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +18,16 @@ import java.util.stream.Collectors;
 public class AccountService {
     private final TradeHistoryRepository tradeHistoryRepository;
     private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
 
     public List<TradeHistoryDto> findAll(long accountId) {
         return tradeHistoryRepository.findByAccountId(accountId)
                 .stream().map(TradeHistory::toDto)
                 .collect(Collectors.toList());
+    }
+
+    public AccountDto findAccount(long userId) {
+        User user = userRepository.findById(userId).get();
+        return user.getAccount().toDto();
     }
 }
