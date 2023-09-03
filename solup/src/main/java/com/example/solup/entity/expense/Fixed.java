896fd5ae2d19e5429f4a71ec268d4b94eefa95d5
woodong27 <cdw5548@gmail.com>
2023-09-03 17:37:30 +0900

    Feat : Create new entities

---
+++ solup/src/main/java/com/example/solup/entity/expense/Fixed.java
@@ -0,0 +1,31 @@
+package com.example.solup.entity.expense;
+
+import com.example.solup.entity.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// 고정비
+@Getter
+@Entity
+public class Fixed {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private String category;
+
+    @Column
+    private LocalDateTime date;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
