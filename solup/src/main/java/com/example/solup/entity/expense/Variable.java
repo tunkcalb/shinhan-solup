f3955ecdd54ee02f08ddbf341bdb2b158ecd6fbb
woodong27 <cdw5548@gmail.com>
2023-09-03 21:10:41 +0900

    Feat : Update entities

--- solup/src/main/java/com/example/solup/entity/expense/Variable.java
+++ solup/src/main/java/com/example/solup/entity/expense/Variable.java
@@ -25,7 +25,7 @@ public class Variable {
     @Column
     private LocalDateTime date;
 
-    @OneToMany(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
