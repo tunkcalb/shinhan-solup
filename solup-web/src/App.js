be19b3650259dd06b13a165d997a8f881f4223f6
노창현 <iach84@yonsei.ac.kr>
2023-09-03 18:40:04 +0900

    Chore: Modify App.js

--- solup-web/src/App.js
+++ solup-web/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Home from 'solup-web/src/pages/Home.js';
+import Login from 'C:/Users/SSAFY/Desktop/solup-web/src/pages/Login.js';
+import Header from 'C:/Users/SSAFY/Desktop/solup-web/src/components/Header.js';
+import logoImage from 'C:/Users/SSAFY/Desktop/solup-web/src/assets/solup logo-blue.png';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header logo={logoImage} />
+    <Router>
+      <Switch>
+        <Route path="/login">
+          {isLoggedIn ? <Redirect to="/" /> : <Login onLogin={handleLogin} />}
+        </Route>
+        <Route path="/">
+          {isLoggedIn ? (
+            <Home onLogout={handleLogout} />
+            ) : (
+              <Redirect to="/login" />
+              )}
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
