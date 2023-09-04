21afc77595674d4e8f9a9088db99642f64cda5b1
stubborngastropod <dnchudnjee@gmail.com>
2023-09-05 00:12:21 +0900

    Chore: Updated components

--- solup-web/src/App.js
+++ solup-web/src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from './pages/Home.js';
-import Login from './pages/Login.js';
+import InitialPage from './pages/InitialPage.js';
 import Header from './components/Header.js';
 import logoImage from './assets/solup-logo-blue.png';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
@@ -23,7 +23,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/login">
-          {isLoggedIn ? <Redirect to="/" /> : <Login onLogin={handleLogin} />}
+          {isLoggedIn ? <Redirect to="/" /> : <InitialPage onLogin={handleLogin} />}
         </Route>
         <Route path="/">
           {isLoggedIn ? (
--- solup-web/src/pages/Home.js
+++ solup-web/src/pages/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import AccountInfo from '../components/Home/AccountInfo.js'
+import ProfitStatus from '../components/Home/ProfitStatus.js'
+import ServiceMenu from '../components/Home/ServiceMenu.js'
 
 function Home() {
   return (
     <div className="home-container">
       <h3>신한커피 김싸피 사장님</h3>
-      <h2>사업자 계좌 정보</h2>
-      {/* 사업자 계좌 정보 컴포넌트 */}
-      <h2>우리가게 손익 현황</h2>
-      {/* 우리가게 손익 현황 컴포넌트 */}
-      <h2>간편 메뉴</h2>
-      {/* 간편메뉴 컴포넌트 */}
+        <AccountInfo />
+        <ProfitStatus />
+        <ServiceMenu />
     </div>
   );
 }
