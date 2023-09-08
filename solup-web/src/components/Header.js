e3dcffc7bd54fccb971cfbbd2eeb5f6899b85014
박진희 <wlsl0620@naver.com>
2023-09-08 22:32:06 +0900

    Chore : Delete unnecessary fonts

--- solup-web/src/components/Header.js
+++ solup-web/src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
-function Header({ logoImage, title }) {
-
+function Header ({ title }) {
   return (
-    <div className="header">
-      
+    <div className='header'>
+      <div className='headerContent'>
+        <Link to="/"><img src={`${process.env.PUBLIC_URL}/backBtn.png`} alt="뒤로가기버튼"/></Link>
+        <div className='headerText'>{title}</div>
+      </div>
     </div>
   );
-}
+};
+
 
 export default Header;
