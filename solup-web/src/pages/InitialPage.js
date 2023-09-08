da0976710070a619ca990888e3ab2e6730b2e429
박진희 <wlsl0620@naver.com>
2023-09-09 00:33:59 +0900

    Chore : Header test

--- solup-web/src/pages/InitialPage.js
+++ solup-web/src/pages/InitialPage.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Header from '../components/Header';
-
 function InitialPage() {
   return (
     <div className="initial-page">
-      <Header title="헤더 테스트" />
       <h2>환영합니다</h2>
       <p>로그인하거나 회원가입하세요.</p>
       <Link to="/home">
--- solup-web/src/pages/Signup.js
+++ solup-web/src/pages/Signup.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from '../components/Header';
 
 function Signup () {
     return (
         <div>
-            
+          <Header title="헤더 테스트 완료" /> 
         </div>
-    )
-}
+    );
+};
 
 export default Signup;
