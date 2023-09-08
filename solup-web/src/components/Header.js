11a4b01395634e1917fd303adff53739d1aa464b
박진희 <wlsl0620@naver.com>
2023-09-08 23:40:58 +0900

    Feat : Header component

--- solup-web/src/components/Header.js
+++ solup-web/src/components/Header.js
@@ -5,9 +5,9 @@ import './Header.css'
 function Header ({ title }) {
   return (
     <div className='header'>
-      <div className='headerContent'>
-        <Link to="/"><img src={`${process.env.PUBLIC_URL}/backBtn.png`} alt="뒤로가기버튼"/></Link>
-        <div className='headerText'>{title}</div>
+      <div className='headerContainer'>
+        <Link to="/"><img src={`${process.env.PUBLIC_URL}/backBtn.png`} alt="뒤로가기버튼"/></Link> 
+        <span className='headerText'>{title}</span>
       </div>
     </div>
   );
