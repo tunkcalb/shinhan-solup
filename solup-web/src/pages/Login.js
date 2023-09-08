34d7d5be3c8da98081e24ffba975376f5bd01d74
stubborngastropod <dnchudnjee@gmail.com>
2023-09-09 00:41:01 +0900

    Chore: Login Signup pages

--- solup-web/src/pages/Login.js
+++ solup-web/src/pages/Login.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux'; // useDispatch 임포트
+import { setIsLoggedIn } from '../redux/actions'; // 액션 임포트
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch(); // useDispatch 훅 사용
 
-  const handleLogin = () => {
-    // 여기에서 로그인 로직을 구현하고 상태를 업데이트하세요.
-    // 예를 들어, 서버로 로그인 요청을 보낼 수 있습니다.
+  const handleLogin = (e) => {
+    e.preventDefault(); // 폼 제출 기본 동작 방지
+
+    // 로그인 로직을 구현하고 상태를 업데이트합니다.
+    // 여기에서는 예제로 사용자 이름과 비밀번호를 확인하는 것으로 가정합니다.
+    if (username === 'admin' && password === 'admin') {
+      // Redux를 사용하여 isLoggedIn 상태를 true로 변경
+      dispatch(setIsLoggedIn(true));
+      // /home 페이지로 이동
+      window.location.href = '/home'; // 현재 페이지를 새로고침하면서 이동
+    } else {
+      alert('로그인 실패. 사용자 이름과 비밀번호를 확인하세요.');
+    }
   };
 
   return (
--- solup-web/src/pages/Signup.js
+++ solup-web/src/pages/Signup.js
@@ -1,11 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // react-router-dom의 useNavigate 사용
+import { useDispatch, useSelector } from "react-redux"; // useDispatch 사용
+import { setIsLoggedIn } from "../redux/actions"; // 액션 임포트
 
-function Signup () {
-    return (
-        <div>
-            
-        </div>
-    )
+function Signup() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn); // Redux 스토어의 isLoggedIn 상태를 읽음
+
+  // State to store form values
+  const [formData, setFormData] = useState({
+    name: "",
+    phoneNumber: "",
+    verificationCode: "",
+    username: "", // 아이디 입력란
+    password: "", // 비밀번호 입력란
+    confirmPassword: "", // 비밀번호 확인 입력란
+  });
+
+  // Function to handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Function to handle the "Next" button click
+  const handleNextClick = () => {
+    // 여기에서 아이디 중복 확인 로직을 수행할 수 있습니다.
+    // 이 예제에서는 그냥 다음 페이지로 이동합니다.
+    // Redux를 사용하여 isLoggedIn 값을 true로 변경
+    dispatch(setIsLoggedIn(true));
+
+    // 초기 페이지로 이동
+    navigate('/');
+  };
+
+  return (
+    <div>
+      {/* 아이디 입력란 */}
+      <div>
+        <label htmlFor="username">아이디</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+        />
+      </div>
+      
+      <button onClick={handleNextClick}>아이디 중복확인</button>
+      
+      <div>
+        <label htmlFor="password">비밀번호</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+      </div>
+      
+      <div>
+        <label htmlFor="confirmPassword">비밀번호 확인</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleInputChange}
+        />
+      </div>
+      
+      <button onClick={handleNextClick}>회원가입 완료</button>
+    </div>
+  );
 }
 
 export default Signup;
