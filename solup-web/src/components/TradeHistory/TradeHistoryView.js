16e0cf57d6c4a7c31c7b7625c5255a71fb0615dc
stubborngastropod <dnchudnjee@gmail.com>
2023-09-09 18:48:06 +0900

    Feat: trade history view

---
+++ solup-web/src/components/TradeHistory/TradeHistoryView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TradeHistoryView () {
+    return (
+        <div>
+            <h2>거래내역 조회 컴포넌트</h2>
+        </div>
+    )
+}
+
+export default TradeHistoryView;
