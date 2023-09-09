3fffbe36d19f9e400cce57f0932ed92cb931e41e
stubborngastropod <dnchudnjee@gmail.com>
2023-09-09 18:47:17 +0900

    Feat: change userinfo and accountnumber into component

--- solup-web/src/pages/Home.js
+++ solup-web/src/pages/Home.js
@@ -2,14 +2,16 @@ import React from 'react';
 import AccountInfo from '../components/Home/AccountInfo.js'
 import ProfitStatus from '../components/Home/ProfitStatus.js'
 import ServiceMenu from '../components/Home/ServiceMenu.js'
-
+import UserInfo from '../components/UserInfo.js'
+import AccountNumber from '../components/AccountNumber.js'
 function Home() {
   return (
     <div className="home-container">
-      <h3>신한커피 김싸피 사장님</h3>
-        <AccountInfo />
-        <ProfitStatus />
-        <ServiceMenu />
+      <UserInfo />
+      <AccountNumber />
+      <AccountInfo />
+      <ProfitStatus />
+      <ServiceMenu />
     </div>
   );
 }
