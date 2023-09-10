8f00ee25a872aabb8848fbf4fa951e9b0b5fb70e
stubborngastropod <dnchudnjee@gmail.com>
2023-09-11 01:06:51 +0900

    Stye : change home username into userinfo component

--- solup-web/src/components/UserInfo.js
+++ solup-web/src/components/UserInfo.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-function UserInfo () {
-    return (
-        <div>
-            <h2>안녕하세요, 신한커피 김싸피 사장님</h2>
-        </div>
-    )
+function UserInfo() {
+  const userName = useSelector((state) => state.userName);
+
+  return (
+    <div>
+      <h2>안녕하세요, {userName} 사장님</h2>
+    </div>
+  );
 }
 
 export default UserInfo;
--- solup-web/src/pages/Home.js
+++ solup-web/src/pages/Home.js
@@ -4,19 +4,26 @@ import ProfitStatus from '../components/Home/ProfitStatus.js'
 import ServiceMenu from '../components/Home/ServiceMenu.js'
 import NavBar from '../components/Footer.js';
 import "./Home.css"
-
+import { useSelector } from 'react-redux'
+import UserInfo from '../components/UserInfo.js';
 function Home() {
+  const userId = useSelector((state) => state.userId);
+  const userName = useSelector((state) => state.userName);
+  console.log(userId)
+  console.log(userName)
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn)
   return (
     <div className="homeContainer">
       <div className='homeLogo'>
         <img src={`${process.env.PUBLIC_URL}/solup-logo-blue.png`} alt="쏠업로고"/>
       </div>
       <div className='homeContent'>
-        <h3>신한커피 김싸피 사장님</h3>
-          <AccountInfo />
-          <ProfitStatus />
-          <ServiceMenu />
-          <NavBar />
+        <UserInfo />
+        <AccountInfo />
+        <ProfitStatus />
+        <ServiceMenu />
+        <NavBar />
       </div>
     </div>
   );
