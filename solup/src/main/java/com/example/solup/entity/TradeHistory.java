0a7fba7d742b3c4bbdf8ce06b11cf9f0e554ee3c
woodong27 <cdw5548@gmail.com>
2023-09-11 00:28:52 +0900

    Chore : Entity 수정

--- solup/src/main/java/com/example/solup/entity/TradeHistory.java
+++ solup/src/main/java/com/example/solup/entity/TradeHistory.java
@@ -5,11 +5,14 @@ import com.example.solup.entity.expense.Fixed;
 import com.example.solup.entity.expense.Living;
 import com.example.solup.entity.expense.Surplus;
 import com.example.solup.entity.expense.Variable;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 // 거래내역
 @Entity
@@ -23,13 +26,25 @@ public class TradeHistory {
     @Column
     private LocalDateTime date;
 
-    // 입금 금액
+    // 거래일자
+    @Column(name = "trade_date")
+    private LocalDate tradeDate;
+
+    // 거래시간
+    @Column(name = "trade_time")
+    private LocalTime tradeTime;
+
+    // 적요
     @Column
-    private Integer deposit;
+    private String briefs;
 
     // 출금 긍맥
     @Column
     private Integer withdraw;
+
+    // 입금 금액
+    @Column
+    private Integer deposit;
 
     // 거래 내용
     @Column
@@ -42,6 +57,10 @@ public class TradeHistory {
     // 입금 지출 구분(입금 : 1, 출금 : 2)
     @Column
     private Integer category;
+
+    // 거래점명
+    @Column
+    private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")
