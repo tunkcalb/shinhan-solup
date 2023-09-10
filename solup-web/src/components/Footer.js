aca33beb894f4b9a36e49b91f6c4dd4d0ba1b8d3
박진희 <wlsl0620@naver.com>
2023-09-10 15:36:26 +0900

    Design : Complete navBar

--- solup-web/src/components/Footer.js
+++ solup-web/src/components/Footer.js
@@ -4,25 +4,25 @@ import './Footer.css';
 
 function NavBar () {
   return (
-    <div className="navBar">
+    <div>
       <ul className="navBarList">
-        <Link to="/">
+        <Link to="/" className="navItem">
           <img src={`${process.env.PUBLIC_URL}/대출.png`} alt="대출"/>
           <div>대출</div>
         </Link>
-        <Link to="/">
+        <Link to="/" className="navItem">
           <img src={`${process.env.PUBLIC_URL}/매출.png`} alt="매출"/>
           <div>매출</div>
         </Link>
-        <Link to="/">
+        <Link to="/" className="navItem">
           <img src={`${process.env.PUBLIC_URL}/홈.png`} alt="홈"/>
           <div>홈</div>
         </Link>
-        <Link to="/">
+        <Link to="/" className="navItem">
           <img src={`${process.env.PUBLIC_URL}/직원.png`} alt="직원"/>
           <div>직원</div>
         </Link>
-        <Link to="/">
+        <Link to="/" className="navItem">
           <img src={`${process.env.PUBLIC_URL}/상품.png`} alt="상품"/>
           <div>상품</div>
         </Link>
