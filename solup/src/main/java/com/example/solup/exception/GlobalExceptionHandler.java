fb8cca0f7637b00029e67c3b5a77ca830c7987e8
hyunwoo <wjdgusdndi@gmail.com>
2023-09-12 23:48:42 +0900

    Feat : mattermost notification

--- solup/src/main/java/com/example/solup/exception/GlobalExceptionHandler.java
+++ solup/src/main/java/com/example/solup/exception/GlobalExceptionHandler.java
@@ -34,6 +34,7 @@ public class GlobalExceptionHandler {
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         e.printStackTrace();
         response.setMessage(e.getMessage());
+        notificationManager.sendNotification(e, req.getRequestURI(), getParams(req));
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
