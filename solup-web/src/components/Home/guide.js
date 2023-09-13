0ce7f1a6c654afca5a9421ec92bb6f298aabb9d3
박진희 <wlsl0620@naver.com>
2023-09-13 23:37:51 +0900

    Move : css files

--- solup-web/src/components/Home/guide.js
+++ solup-web/src/components/Home/guide.js
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import "../../pages/Home.css";
+import "../../pages/styles/Home.css";
 import "./Guide.css";
 import BlueButton from "../BlueButton";
 
