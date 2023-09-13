a92f7da96a8a120af4412b88cc9b860187bde3f6
hyunwoo <wjdgusdndi@gmail.com>
2023-09-14 01:28:50 +0900

    Feat : get data

--- solup-web/src/components/RevenueAnalysis/RevenueAnalysisView.js
+++ solup-web/src/components/RevenueAnalysis/RevenueAnalysisView.js
@@ -2,25 +2,47 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-function RevenueAnalysisView () {
-    const isAccountRegistered = useSelector((state) => state.isAccountRegistered);
-    const [accountData, setAccountData] = useState(null);
-    const [RevenueAnalysisData, setRevenueAnalysisData] = useState(null);
-    const userId = useSelector((state) => state.userId);
-    console.log(userId);
-    // const data = axios.get(`http://54.206.147.12/user/revenue/analysis/${userId}`).then(res => {console.log(res.data)})
-    
-    useEffect(() => {
-        const data = axios.get(`http://localhost:8080/user/revenue/analysis/1`).then(res => {setRevenueAnalysisData(res.data.data)})
-    }, [])
-    return (
+function RevenueAnalysisView() {
+  const isAccountRegistered = useSelector((state) => state.isAccountRegistered);
+  const [accountData, setAccountData] = useState(null);
+  const [RevenueAnalysisData, setRevenueAnalysisData] = useState(null);
+  const userId = useSelector((state) => state.userId);
+  console.log(userId);
+
+  useEffect(() => {
+    const data = axios.get(`/user/revenue/analysis/${userId}`).then((res) => {
+      setRevenueAnalysisData(res.data.data);
+    });
+  }, []);
+  return (
+    <div>
+      {RevenueAnalysisData && (
         <div>
-            <h2>이번달 매출</h2>
-            {RevenueAnalysisData && (<p>{RevenueAnalysisData.monthRevenue}</p>)}
+          <p>이번달 매출 {RevenueAnalysisData.monthRevenue}원</p>
+          <p>매출분석 그래프</p>
+          <ul>
+            {Object.entries(RevenueAnalysisData.monthlyRevenue).map(
+              ([key, value]) => (
+                <li key={key}>
+                  {key} : {value}
+                </li>
+              )
+            )}
+          </ul>
+          <p>카드 매출 분석</p>
+          <ul>
+            {Object.entries(RevenueAnalysisData.cardRevenue).map(
+              ([key, value]) => (
+                <li key={key}>
+                  {key}: {value}
+                </li>
+              )
+            )}
+          </ul>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default RevenueAnalysisView;
-
-
