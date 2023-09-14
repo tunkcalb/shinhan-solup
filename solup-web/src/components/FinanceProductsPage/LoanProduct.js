94b736532a8880d0f5ade0eb46e69c52c94a458d
stubborngastropod <dnchudnjee@gmail.com>
2023-09-15 04:06:51 +0900

    Fix : playstore link fix

--- solup-web/src/components/FinanceProductsPage/LoanProduct.js
+++ solup-web/src/components/FinanceProductsPage/LoanProduct.js
@@ -3,8 +3,18 @@ import React from 'react';
 function LoanProduct({ product }) {
   const handleApplyLoan = () => {
     const solAppPackage = 'com.shinhan.sbanking';
-    const playStoreUrl = `https://play.google.com/store/apps/details?id=${solAppPackage}`;
-    window.open(playStoreUrl, '_blank');
+
+    // Android
+    if (navigator.userAgent.match(/Android/i)) {
+      window.location.href = `market://details?id=${solAppPackage}`;
+    }
+    
+    // 모바일 브라우저
+    else {
+      const playStoreUrl = `https://play.google.com/store/apps/details?id=${solAppPackage}`;
+      window.open(playStoreUrl, '_blank');
+      console.log('다른 플랫폼 또는 데스크톱 브라우저에서 열림');
+    }
   };
 
   return (
