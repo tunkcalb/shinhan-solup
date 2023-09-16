9d536b415d0fb87894167469d7b27a5b5d48a0af
woodong27 <cdw5548@gmail.com>
2023-09-17 02:44:58 +0900

    Chore : 주석 추가

--- solup/src/main/java/com/example/solup/service/account/AccountService.java
+++ solup/src/main/java/com/example/solup/service/account/AccountService.java
@@ -85,8 +85,6 @@ public class AccountService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("해당 회원을 찾을 수 없습니다."));
 
-        Long accountId = user.getAccount().getId();
-
         String briefs = request.getBriefs();
         String content = request.getContent();
         String expenseType = request.getExpenseType();
@@ -95,6 +93,7 @@ public class AccountService {
         List<TradeHistory> tradeHistories = tradeHistoryRepository.findByBriefsAndContentAndCategoryAndIsCategorized(
                 briefs, content, 2, false);
 
+        // expenseCategory가 Fixed(고정비일 경우)
         if (Objects.equals(expenseType, "Fixed")) {
             Fixed fixed = new Fixed();
             fixed.setCategory(expenseCategory);
@@ -105,6 +104,8 @@ public class AccountService {
                 tradeHistory.setFixed(fixed);
                 tradeHistory.setIsCategorized(true);
             });
+
+        // expenseCategory가 Variable(변동비일 경우)
         } else if (Objects.equals(expenseType, "Variable")) {
             Variable variable = new Variable();
             variable.setCategory(expenseCategory);
