d246f2a021c3a95714c1ada3443443de59aeb494
woodong27 <cdw5548@gmail.com>
2023-09-17 05:44:44 +0900

    Feat : 로그인/회원가입 코드 수정 및 로직 추가

--- solup-web/src/pages/Verification.js
+++ solup-web/src/pages/Verification.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import Header from "../components/Header";
 import BlueButton from "../components/BlueButton";
-import "./styles/Verification.css"
+import "./styles/Verification.css";
 
 function Verification() {
   const navigate = useNavigate();
@@ -12,6 +12,7 @@ function Verification() {
     phoneNumber: "",
     verificationCode: "",
   });
+  const [destination, setDestination] = useState("");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -23,22 +24,42 @@ function Verification() {
 
   const handleSendVerificationCode = async () => {
     try {
-      const response = await axios.post('/user/sms', {
+      const response = await axios.post("/user/sms", {
         recipientPhoneNumber: formData.phoneNumber,
       });
 
-      if (response.data.code === 'success') {
-        alert('인증번호를 전송했습니다.');
+      if (response.data.code === "200") {
+        alert("인증번호를 전송했습니다.");
       } else {
-        alert('인증번호 전송에 실패했습니다.');
+        alert("인증번호 전송에 실패했습니다.");
       }
     } catch (error) {
-      console.error('인증번호 전송 오류:', error);
+      console.error("인증번호 전송 오류:", error);
     }
   };
 
-  const handleNextButtonClick = () => {
-    navigate(`/signup?name=${formData.name}&phoneNumber=${formData.phoneNumber}`);
+  // const handleNextButtonClick = () => {
+  //   navigate(
+  //     `/signup?name=${formData.name}&phoneNumber=${formData.phoneNumber}`
+  //   );
+  // };
+
+  const updateDestination = () => {
+    if (
+      formData.name === "" ||
+      formData.phoneNumber === "" ||
+      formData.verificationCode === ""
+    ) {
+      alert("모든 항목을 채워주세요");
+      setDestination("/verification");
+    } else {
+      setDestination(
+        `/signup?name=${formData.name}&phoneNumber=${formData.phoneNumber}`
+      );
+      navigate(
+        `/signup?name=${formData.name}&phoneNumber=${formData.phoneNumber}`
+      );
+    }
   };
 
   return (
@@ -46,10 +67,11 @@ function Verification() {
       <Header title="회원가입" />
       <div className="container">
         <div className="contents">
-
           <div className="subTitle">본인인증을 진행해주세요</div>
           <div className="inputForm">
-            <label htmlFor="name" className="inputTitle">이름</label>
+            <label htmlFor="name" className="inputTitle">
+              이름
+            </label>
             <input
               type="text"
               id="name"
@@ -61,7 +83,9 @@ function Verification() {
           </div>
 
           <div className="inputForm">
-            <label htmlFor="phoneNumber" className="inputTitle">전화번호</label>
+            <label htmlFor="phoneNumber" className="inputTitle">
+              전화번호
+            </label>
             <input
               type="text"
               id="phoneNumber"
@@ -70,11 +94,15 @@ function Verification() {
               onChange={handleInputChange}
               className="inputContent"
             />
-            <button onClick={handleSendVerificationCode} className="whiteBtn">인증번호 발송</button>
+            <button onClick={handleSendVerificationCode} className="whiteBtn">
+              인증번호 발송
+            </button>
           </div>
 
           <div className="inputForm">
-            <label htmlFor="verificationCode" className="inputTitle">인증번호</label>
+            <label htmlFor="verificationCode" className="inputTitle">
+              인증번호
+            </label>
             <input
               type="text"
               id="verificationCode"
@@ -84,9 +112,9 @@ function Verification() {
               className="inputContent"
             />
           </div>
-          
-          <div className="verificationBtnContainer">
-            <BlueButton title="다음" destination={`/signup?name=${formData.name}&phoneNumber=${formData.phoneNumber}`} />
+
+          <div className="verificationBtnContainer" onClick={updateDestination}>
+            <BlueButton title="다음" destination={destination} />
           </div>
         </div>
       </div>
